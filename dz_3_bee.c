bdc51477f1d55c0b8cd9b72be71c0cd809d28d4e
Andrew-Malchenko <119253720+Andrew-Malchenko@users.noreply.github.com>
2023-06-14 02:50:38 +0300

    Update dz_3_bee.c

--- dz_3_bee.c
+++ dz_3_bee.c
@@ -5,8 +5,6 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-#define SERVER_IP "127.0.0.1"
-#define PORT 8888
 #define BUFFER_SIZE 1024
 
 void error(const char *message) {
@@ -20,17 +18,14 @@ int main(int argc, char **argv) {
     struct sockaddr_in server_address;
     char buffer[BUFFER_SIZE];
 
-    // Create client socket
     if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         error("Failed to create socket");
     }
 
-    // Configure server address
     server_address.sin_family = AF_INET;
     server_address.sin_addr.s_addr = inet_addr(argv[1]);
     server_address.sin_port = htons(atoi(argv[2]));
 
-    // Connect to the server
     if (connect(client_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
         error("Failed to connect to the server");
     }
@@ -42,14 +37,12 @@ int main(int argc, char **argv) {
         printf("Press ENTER to collect honey or type 'exit' to quit: ");
         fgets(buffer, BUFFER_SIZE, stdin);
 
-        // Remove newline character
         buffer[strcspn(buffer, "\n")] = 0;
 
         if (strcmp(buffer, "exit") == 0) {
             break;
         }
 
-        // Send "collect_honey" message to the server
         if (send(client_socket, "collect_honey", strlen("collect_honey"), 0) < 0) {
             error("Failed to send message to the server");
         }
